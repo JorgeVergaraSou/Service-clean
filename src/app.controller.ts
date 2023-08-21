685c354a1377c07dfa3e7341526cb8b16264fcc2
jorge vergara <jorgevergarasou@gmail.com>
2023-08-21 19:13:35 -0300

    mount BD

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { join } from 'path';
 
-@Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
+@Controller('cleanServ')
+export class CleanController {
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getSongs(@Res() res: Response) {
+    const cleanServ = join(__dirname, '../data/services.json')
+    res.sendFile(cleanServ)
   }
+
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CleanController } from './app.controller';
+
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [CleanController],
+  providers: [],
 })
 export class AppModule {}
